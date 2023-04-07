421da4668d04e866c6b99ae7c6bfa55dc2499a8d
Mat-tapella <matheo.tapella@gmail.com>
2023-04-07 10:28:05 +0200

    First api

--- meteo/App.js
+++ meteo/App.js
@@ -1,18 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import WeatherData from './Component/CurrentWeatherData';
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat={45.81660461425781}&lon={4.8998847007751465}&appid=b0c846745fe0887c91d1ed6dfd4a0719`)
+      .then(response => response.json())
+      .then(data => {
+        setWeatherData(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Bienvenue</Text>
-      <Text>Voici la météo du jour</Text>
-      <WeatherData />
-      <StatusBar style="auto" />
+      {weatherData ? (
+        <View>
+          <Text>Ville: {weatherData.name}</Text>
+          <Text>Météo: {weatherData.weather[0].description}</Text>
+          <Text>Température: {weatherData.main.temp}</Text>
+        </View>
+      ) : (
+        <Text>Chargement...</Text>
+      )}
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
